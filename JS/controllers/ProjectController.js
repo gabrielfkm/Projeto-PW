64d2654bfb59cd4af866ab9c0c873a999082e249
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 20:19:57 -0300

    Update ProjectController.js

--- JS/controllers/ProjectController.js
+++ JS/controllers/ProjectController.js
@@ -1,1 +1,20 @@
+import { ProjectModel } from '../models/ProjectModel.js';
+import { ProjectView } from '../views/ProjectView.js';
 
+export const ProjectController = {
+  async init() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (!user) return window.location.href = '../pages/login.html';
+
+    const projects = await ProjectModel.getByUserId(user.id);
+    ProjectView.renderProjects(projects);
+  },
+
+  async create(formData) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const newProject = { ...formData, userId: user.id };
+    await ProjectModel.create(newProject);
+    ProjectView.showCreateSuccess();
+    this.init(); // Recarrega lista
+  }
+};
