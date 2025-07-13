ec3d65357442229e25a5df26fb87ba3114816c0a
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 19:33:17 -0300

    Update UserService.js

--- JS/services/UserService.js
+++ JS/services/UserService.js
@@ -1,1 +1,17 @@
+const BASE_URL = 'https://6873e2e0c75558e273558c87.mockapi.io/projeto/user';
 
+export const UserService = {
+  async getAll() {
+    const res = await fetch(`${BASE_URL}/users`);
+    return res.json();
+  },
+
+  async create(user) {
+    const res = await fetch(`${BASE_URL}/users`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    });
+    return res.json();
+  }
+};
