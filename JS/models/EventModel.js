8b71a618e6c332c56faa022b71a820ceab11fe37
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 19:48:56 -0300

    Update EventModel.js

--- JS/models/EventModel.js
+++ JS/models/EventModel.js
@@ -1,1 +1,16 @@
+import { EventService } from '../services/eventService.js';
 
+export const EventModel = {
+  async getAll() {
+    return await EventService.getAll();
+  },
+
+  async getByUserId(userId) {
+    const events = await EventService.getAll();
+    return events.filter(event => event.userId === userId);
+  },
+
+  async create(event) {
+    return await EventService.create(event);
+  }
+};
