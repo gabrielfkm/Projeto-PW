f186eae6d2b0abf82bc01118f397edc203f5dae1
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 19:46:59 -0300

    Update TaskModel.js

--- JS/models/TaskModel.js
+++ JS/models/TaskModel.js
@@ -1,1 +1,16 @@
+import { TaskService } from '../services/taskService.js';
 
+export const TaskModel = {
+  async getAll() {
+    return await TaskService.getAll();
+  },
+
+  async getByProjectId(projectId) {
+    const tasks = await TaskService.getAll();
+    return tasks.filter(task => task.projectId === projectId);
+  },
+
+  async create(task) {
+    return await TaskService.create(task);
+  }
+};
