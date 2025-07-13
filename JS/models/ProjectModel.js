093ca75898fd0cb5604c0933551181d586dd08cf
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 19:45:17 -0300

    Update ProjectModel.js

--- JS/models/ProjectModel.js
+++ JS/models/ProjectModel.js
@@ -1,1 +1,16 @@
+import { ProjectService } from '../services/projectService.js';
 
+export const ProjectModel = {
+  async getAll() {
+    return await ProjectService.getAll();
+  },
+
+  async getByUserId(userId) {
+    const all = await ProjectService.getAll();
+    return all.filter(p => p.userId === userId);
+  },
+
+  async create(project) {
+    return await ProjectService.create(project);
+  }
+};
