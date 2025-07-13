83c715dfeb792c4b1ac654976e4f280675062839
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 20:07:55 -0300

    Update TaskView.js

--- JS/views/TaskView.js
+++ JS/views/TaskView.js
@@ -1,1 +1,22 @@
+export const TaskView = {
+  renderTasks(tasks) {
+    const container = document.getElementById('task-list');
+    container.innerHTML = '';
 
+    if (tasks.length === 0) {
+      container.innerHTML = '<p>Nenhuma tarefa encontrada.</p>';
+      return;
+    }
+
+    tasks.forEach(task => {
+      const item = document.createElement('div');
+      item.className = 'task-item';
+      item.innerHTML = `
+        <h4>${task.title}</h4>
+        <p>Status: ${task.status}</p>
+        <p>Entrega: ${task.dueDate}</p>
+      `;
+      container.appendChild(item);
+    });
+  }
+};
