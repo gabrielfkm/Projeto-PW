a58afd6bb1c55e24a5f72e25376c2c2c3c630ede
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 20:17:26 -0300

    Update TaskController.js

--- JS/controllers/TaskController.js
+++ JS/controllers/TaskController.js
@@ -1,1 +1,14 @@
+import { TaskModel } from '../models/TaskModel.js';
+import { TaskView } from '../views/TaskView.js';
 
+export const TaskController = {
+  async init(projectId) {
+    const tasks = await TaskModel.getByProjectId(projectId);
+    TaskView.renderTasks(tasks);
+  },
+
+  async create(formData, projectId) {
+    await TaskModel.create({ ...formData, projectId });
+    this.init(projectId);
+  }
+};
