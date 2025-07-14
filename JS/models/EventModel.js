e2c5d30a906efaada52a0aa8d690d021ab7b665e
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-13 22:55:35 -0300

    Update EventModel.js

--- JS/models/EventModel.js
+++ JS/models/EventModel.js
@@ -1,16 +1,36 @@
-import { EventService } from '../services/eventService.js';
+const EVENT_API = 'https://68742e5bdd06792b9c93509d.mockapi.io/project/api/events';
 
 export const EventModel = {
   async getAll() {
-    return await EventService.getAll();
+    const res = await fetch(EVENT_API);
+    return await res.json();
   },
 
-  async getByUserId(userId) {
-    const events = await EventService.getAll();
-    return events.filter(event => event.userId === userId);
+  async getByDate(date) {
+    const res = await fetch(`${EVENT_API}?date=${date}`);
+    return await res.json();
   },
 
   async create(event) {
-    return await EventService.create(event);
+    const res = await fetch(EVENT_API, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(event)
+    });
+    return await res.json();
+  },
+
+  async update(id, updatedEvent) {
+    const res = await fetch(`${EVENT_API}/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedEvent)
+    });
+    return await res.json();
+  },
+
+  async delete(id) {
+    const res = await fetch(`${EVENT_API}/${id}`, { method: 'DELETE' });
+    return res.ok;
   }
 };
